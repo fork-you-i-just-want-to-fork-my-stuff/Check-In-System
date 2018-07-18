22cb683c5993df74e4b93476acc0d90047842bdd
Kieran Wood <kieranw098@gmail.com>
2018-07-17 21:09:40 -0600

    Updated database rows

--- dbUtilities.py
+++ dbUtilities.py
@@ -5,8 +5,8 @@ def get_client_db(user = None):
     #if you want the whole database returned
     client_db = lite.connect('clients.db')
     if user == None:
-        current = client_db.execute('SELECT client_name, phone_number, postal_code, email from clients')
-        clients = [dict(client_name=row[0], phone_number=row[1], postal_code=row[2], email=row[3]) for row in current.fetchall()]
+        current = client_db.execute('SELECT client_first_name, client_last_name, phone_number, postal_code, email from clients')
+        clients = [dict(client_first_name=row[0], client_last_name=row[1],phone_number=row[2], postal_code=row[3], email=row[4]) for row in current.fetchall()]
         client_db.close()
         return clients
 
@@ -18,10 +18,10 @@ def get_client_db(user = None):
 if __name__ == "__main__":
     # Creating test data for client db
     clients = (
-        ("James", "(403)123-1234", "T2Z 3l9", "bazinga@gmail.com"),
-        ("Dianne", "(403)692-1234", "T2Z 5l9", "ww@gmail.com"),
-        ("Mark", "(403)616-1234", "R3Z 3l9", "sdaklfj@gmail.com"),
-        ("Linda", "(403)231-1234", "Y4P 3l9", "asdlfkyhqw@gmail.com")
+        ("James","McTavish", "(403)123-1234", "T2Z 3l9", "bazinga@gmail.com"),
+        ("Dianne", "Lawsorman", "(403)692-1234", "T2Z 5l9", "ww@gmail.com"),
+        ("Mark", "Robertson", "(403)616-1234", "R3Z 3l9", "sdaklfj@gmail.com"),
+        ("Linda", "Saursour", "(403)231-1234", "Y4P 3l9", "asdlfkyhqw@gmail.com")
     )
 
     #seting up client database
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     with con:
         current = con.cursor()
         current.execute("DROP TABLE IF EXISTS clients")
-        current.execute("CREATE TABLE clients(client_name TEXT, phone_number TEXT, postal_code TEXT, email TEXT)")
-        current.executemany("INSERT INTO clients VALUES(?,?,?,?)", clients)
+        current.execute("CREATE TABLE clients(client_first_name TEXT, client_last_name TEXT, phone_number TEXT, postal_code TEXT, email TEXT)")
+        current.executemany("INSERT INTO clients VALUES(?,?,?,?,?)", clients)
